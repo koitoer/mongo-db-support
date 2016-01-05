587e9defb82cf6a7b13bdf351be84f7b7c61cfba
koitoer <koitoer@gmail.com>
2016-01-05 12:59:56 -0700

    [ADD] Add spark and freemarker examples

--- src/main/java/com/koitoer/uni/mongodb/web/HelloWorldSpark.java
+++ src/main/java/com/koitoer/uni/mongodb/web/HelloWorldSpark.java
@@ -6,10 +6,12 @@ import spark.Route;
 import spark.Spark;
 
 /**
+ * Just a hello world to introduce spark framework
  * Created by mauricio.mena on 01/12/2015.
  */
 public class HelloWorldSpark {
 
+	//http://localhost:4567/
 	public static void main(String[] args) {
 		Spark.get(new Route("/") {
 
